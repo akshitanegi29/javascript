61880763c9cc7353c031f5c95c86a1d27da25477
akshitanegi29 <148643003+akshitanegi29@users.noreply.github.com>
2024-02-24 18:47:45 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -14,5 +14,37 @@ let isLoggedIn = true
 
 //NON-premitive DT
 //Object
+//Arrays
+//Functions
 
 console.log(typeof null);
+
+
+
+/*
+1.Number: Represents both integer and floating-point numbers. Example: 5, 3.14.
+ 
+2.String: Represents a sequence of characters enclosed in single or double quotes. Example: "Hello, world!".
+
+3,Boolean: Represents a binary value, either true or false, often used for conditional logic.
+
+4.Undefined: Represents a variable that has been declared but hasn't been assigned a value yet. Example: let x;.
+
+5.Null:  standalone value it is represenataion of empty value it is special type and it is a object type. Example: let y = null; 
+
+6.Symbol (ES6): it is mostly used used to find uniqnece.
+
+7.BigInt (ES11): Represents large integers that cannot be represented by the Number type.
+
+ Refence (Object Data Types):
+
+1.Object: Represents a collection of key-value pairs (properties and methods). Example: { name: "John", age: 30 }.
+
+2.Array: Represents a list-like collection of values, indexed by numbers (integer indices). Example: [1, 2, 3, 4].
+
+3.Function: Represents a reusable block of code that can be invoked or called with arguments. Example: function add(x, y) { return x + y; }.
+
+4.Date: Represents dates and times.
+
+5.RegExp: Represents regular expressions for pattern matching.
+*/
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 2);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("2" == 2);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log(1 == !null);
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Equality
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Greater_than_or_equal
