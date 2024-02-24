fc03b7d011b3841e8d8d358f49580f80cda6f977
akshitanegi29 <148643003+akshitanegi29@users.noreply.github.com>
2024-02-24 14:53:29 +0000

    variables and data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //script mode: It helps you to write cleaner code, like prevents you from using undeclared variables, throws error
+//alert(1+1) we are using node and not browser
+let name = 'Akshita'
+let age = 25
+let isLoggedIn = true
+//premitive DT
+//Number => it can only safely store integers in the range 2^53
+//BigInt
+//String
+//Boolean => true/false
+//null => standalone value
+//undefined
+//Symbol => unique
+
+//NON-premitive DT
+//Object
+
+console.log(typeof null);
