e897a24b79618c5a1470139a7b24340be2258070
akshitanegi29 <148643003+akshitanegi29@users.noreply.github.com>
2024-03-02 15:03:16 +0000

    how js works

---
+++ 03_basics/05_jsWorks.js
@@ -0,0 +1,18 @@
+//run code in browser by adding new snippet
+let a = 10;
+var x = 7;
+function name(){
+console.log('Akshita')
+}
+const value = (c) => {
+    let b = c+c;
+    return b;
+}
+let result = value(a);
+console.log(result);
+name();
+/*
+learn about diff scope creation for let/const and var and functions
+let and const are in script scope
+var in global scopal and hence can be accessed using this
+*/
