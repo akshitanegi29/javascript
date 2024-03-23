84d81ff045eb2bbc2fb2e65ac3cc05d87919932e
akshitanegi29 <148643003+akshitanegi29@users.noreply.github.com>
2024-03-23 20:14:11 +0000

    call

--- 09_classes_oop/04_call.js
+++ 09_classes_oop/04_call.js
@@ -12,3 +12,15 @@ function createUser(username, email, age){
 }
 const user1 = new createUser('akshita', 'an@gmail.com', '25');
 console.log(user1)
+
+//------- example 2 ------
+let name1 = {
+    firstname: "Akshita",
+    lastName: "negi"
+}
+let print = function(hometown){
+    console.log(this.firstname+" "+this.lastName+" lives in "+hometown)
+}
+
+print.call(name1, "UK") //first arg is the reference of obj you want to point to
+//function borrowing
